5567978bba8b15847169aebb96906aa1a3c4d15c
Naveen Ramesh <45918807+fantasticpanthom@users.noreply.github.com>
2020-10-09 11:31:25 +0530

    added concurrency

--- pandafuzz.go
+++ pandafuzz.go
@@ -1,49 +1,62 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
-    "log"
-    "net/http"
-    "bufio"
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
 )
 
 func main() {
-    required := []string{"url", "wl"}
-    url := flag.String("url", "", "Target URL")
-    wordlist := flag.String("wl","","Wordlist file path")
+	required := []string{"url", "wl"}
+	url := flag.String("url", "", "Target URL")
+	wordlist := flag.String("wl", "", "Wordlist file path")
+	fuzzword := flag.String("fw", "fuzz", "fuzz keyword")
+	flag.Parse()
+	seen := make(map[string]bool)
+	flag.Visit(func(f *flag.Flag) { seen[f.Name] = true })
+	for _, req := range required {
+		if !seen[req] {
+			fmt.Fprintf(os.Stderr, "missing required -%s argument/flag\n", req)
+			os.Exit(2)
+		}
+	}
+	file, err := os.Open(*wordlist)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    flag.Parse()
+	jobs := make(chan string)
+	var wg sync.WaitGroup
 
-    seen := make(map[string]bool)
-    flag.Visit(func(f *flag.Flag) { seen[f.Name] = true })
-    for _, req := range required {
-        if !seen[req] {
-            fmt.Fprintf(os.Stderr, "missing required -%s argument/flag\n", req)
-            os.Exit(2)
-        }
-    }
+	for i := 0; i < 20; i++ {
 
-    
-    file, err := os.Open(*wordlist)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+		wg.Add(1)
 
+		go func() {
+			defer wg.Done()
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        resp, err := http.Get(*url+"/"+scanner.Text())
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(*url+"/"+scanner.Text(),":", resp.StatusCode)
-    }
+			for URL := range jobs {
+				resp, err := http.Get(URL)
+				if err != nil {
+					continue
+				}
+				fmt.Println(URL, ":", resp.StatusCode)
+			}
+		}()
+	}
 
-    
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
-    }
+	sc := bufio.NewScanner(file)
+	for sc.Scan() {
+		URL := strings.ReplaceAll(*url, *fuzzword, sc.Text())
+		jobs <- URL
+	}
+	close(jobs)
+
+	wg.Wait()
 }
